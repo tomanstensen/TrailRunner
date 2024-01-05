bce438655fdfc2b4acfdfb8c16169ec38a7e2d55
Johan Carlberg <johan.hansson.carlberg@gmail.com>
2024-01-05 10:51:48 +0100

    Add tests for creating activity and checking that activity has id string

---
+++ src/main/java/se/iths/Activity.java
@@ -0,0 +1,29 @@
+package se.iths;
+
+import java.time.Duration;
+import java.util.Calendar;
+
+public class Activity {
+    public static long idCounter = 1;
+    double distance;
+    Duration time;
+    Calendar date;
+    String id;
+
+    public Activity(double distance, int time, Calendar date){
+        this.distance = distance;
+        this.time = Duration.ofSeconds(time);
+        this.date = date; 
+        this.id = Long.toString(idCounter++);
+    }
+
+    public Activity(double distance, int hours, int minutes, int seconds, Calendar date){
+        Duration tempTime = Duration.ofHours(hours);
+        tempTime = tempTime.plusMinutes(minutes);
+        tempTime = tempTime.plusSeconds(seconds);
+        this.distance = distance;
+        this.time = tempTime;
+        this.date = date; 
+        this.id = Long.toString(idCounter++);
+    }
+}
---
+++ src/test/java/se/iths/ActivityTest.java
@@ -0,0 +1,35 @@
+package se.iths;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.GregorianCalendar;
+
+public class ActivityTest {
+
+    @Test
+    public void createActivity() {
+        Activity activity = new Activity(1, 60, new GregorianCalendar(2024, 1, 1));
+
+        assertEquals(1, activity.distance);
+        assertEquals(60, activity.time.getSeconds());
+        assertEquals(new GregorianCalendar(2024, 1, 1), activity.date);
+    }
+
+    @Test
+    public void activityHasIdString() {
+        Activity activity = new Activity(1, 60, new GregorianCalendar(2024, 1, 1));
+        
+        assertEquals("1", activity.id);
+
+        Activity activity2 = new Activity(1, 60, new GregorianCalendar(2024, 1, 1));
+
+        assertEquals("2", activity2.id);
+    }
+    
+    @Test
+    public void createActivityInHourMinuteSecond() {
+        Activity activity = new Activity(1, 1, 2, 3, new GregorianCalendar(2024, 1, 1));
+
+        assertEquals(3723, activity.time.getSeconds());
+    }
+}
